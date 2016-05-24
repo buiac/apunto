5e9a2147569ac33fdf585cf616d85bc7d49debf9
Sebi Kovacs <sebi.kovacs@gmail.com>
2016-05-24 17:56:53 +0300

    update

--- .openshift/cron/minutely/sender.sh
+++ .openshift/cron/minutely/sender.sh
@@ -7,5 +7,4 @@ fi
 if [ -z "$OPENSHIFT_NODEJS_PORT" ]; then
     OPENSHIFT_NODEJS_PORT="8080"
 fi
-
-curl $OPENSHIFT_NODEJS_IP:$OPENSHIFT_NODEJS_PORT"/api/1/events/r/remind"
+curl $OPENSHIFT_NODEJS_IP:$OPENSHIFT_NODEJS_PORT"/api/1/events/r/remind/"
